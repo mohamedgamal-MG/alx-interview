2cf38b64f82ef854752b2145bdb19a54b2a740b2
Mohamed Gamal <120610309+mohamedgamal-MG@users.noreply.github.com>
2024-01-21 22:55:06 +0200

    updata

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-request(apiUrl, (error, response, body) => {
-  if (error || response.statusCode !== 200) {
-    console.error('Error fetching movie information');
-    process.exit(1);
-  }
-  const characters = JSON.parse(body).characters;
-  function printCharacterNames(characterUrls, index = 0) {
-    if (index === characterUrls.length) {
-
-      process.exit(0);
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
     }
-    request(characterUrls[index], (charError, charResponse, charBody) => {
-      if (charError || charResponse.statusCode !== 200) {
-        console.error(`Error fetching character ${index + 1} information`);
-        process.exit(1);
-      }
-      const characterName = JSON.parse(charBody).name;
-      console.log(characterName);
-      printCharacterNames(characterUrls, index + 1);
-    });
-  }
-  printCharacterNames(characters);
-});
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
