614ff6ec09277da9d7ab6436afc9e83f8441e275
Mohamed Gamal <120610309+mohamedgamal-MG@users.noreply.github.com>
2024-01-21 22:42:01 +0200

    starwar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,32 @@
 #!/usr/bin/node
 const request = require('request');
 
-const endpoint = 'https://swapi-api.hbtn.io/api';
-const filmId = process.argv[2];
-
-request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
-  if (error) return console.log(error);
-
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+request(apiUrl, (error, response, body) => {
+  if (error || response.statusCode !== 200) {
+    console.error('Error fetching movie information');
+    process.exit(1);
+  }
   const characters = JSON.parse(body).characters;
+  function printCharacterNames(characterUrls, index = 0) {
+    if (index === characterUrls.length) {
 
-  for (const character of characters) {
-    await new Promise((resolve, reject) => {
-      request(character, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          console.log(JSON.parse(body).name);
-          resolve(body);
-        }
-      });
+      process.exit(0);
+    }
+    request(characterUrls[index], (charError, charResponse, charBody) => {
+      if (charError || charResponse.statusCode !== 200) {
+        console.error(`Error fetching character ${index + 1} information`);
+        process.exit(1);
+      }
+      const characterName = JSON.parse(charBody).name;
+      console.log(characterName);
+      printCharacterNames(characterUrls, index + 1);
     });
   }
+  printCharacterNames(characters);
 });
